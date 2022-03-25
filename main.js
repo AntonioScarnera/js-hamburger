2dd8bf7b3adfe257f09c768b5f4d3c37b651d77d
AntonioScarnera <scarnerantonio07@gmail.com>
2022-03-25 16:54:41 +0100

    esercizio

---
+++ main.js
@@ -0,0 +1,12 @@
+let burgerOpen = document.querySelector('.header-right > a');
+console.log(burgerOpen);
+
+let burgerMenu = document.querySelector('.hamburger-menu');
+console.log(burgerMenu);
+
+burgerOpen.addEventListener("cilck", myFunction);
+
+function myFunction(){
+    //document.querySelector('.hamburger-menu').classList.add(".active");
+    console.log('prova');
+}
